1f786a5dc26f52171ec9eab21ed540b3ebf3653a
glowvaiteam <glowvai.team@gmail.com>
2025-12-27 20:06:56 +0530

    edit

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
 
-cred = credentials.Certificate("app/core/firebase_key.json")
+cred = credentials.Certificate("firebase_key")
 
 firebase_admin.initialize_app(cred)
 
