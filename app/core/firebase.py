7957d1fc9d0ef3f4ba161d683d15376a1986c460
glowvaiteam <glowvai.team@gmail.com>
2025-12-27 20:36:09 +0530

    edit

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -1,19 +1,9 @@
-import os
-import json
-import firebase_admin
+import json, os, firebase_admin
 from firebase_admin import credentials, firestore
 
-# Read Firebase key from Render environment variable
-firebase_key = os.environ.get("firebase_key")
+firebase_json = os.getenv("FIREBASE_SERVICE_ACCOUNT")
 
-if not firebase_key:
-    raise Exception("firebase_key environment variable is missing")
-
-# Convert string JSON → dict
-cred_dict = json.loads(firebase_key)
-
-# Initialize Firebase
-cred = credentials.Certificate(cred_dict)
+cred = credentials.Certificate(json.loads(firebase_json))
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
